d64a38e21aef522ac260de090299c424c26e91d7
Cicero Teixeira <ccrtxr@gmail.com>
2023-03-14 13:50:45 +1030

    Initial configuration

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, console.log(`Server running on port ${port}`));
